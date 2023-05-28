2b5b320ff5b9fc78673b631ea1f60c076f5dc2ab
oh-taehyun <xogus7038@naver.com>
2023-05-28 23:11:38 +0900

    삼각형의 완성조건(1)

---
+++ codingtest/programmers/src/lv0/삼각형의완성조건.java
@@ -0,0 +1,14 @@
+package lv0;
+import java.util.*;
+public class 삼각형의완성조건 {
+	 public int solution(int[] sides) {
+	        int answer = 0;
+	        Arrays.sort(sides);
+	        if((sides[0]+sides[1])>sides[2]){
+	            answer=1;
+	        }else{
+	            answer=2;
+	        }
+	        return answer;
+	    }
+}
