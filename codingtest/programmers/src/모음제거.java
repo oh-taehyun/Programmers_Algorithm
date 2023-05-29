e86a2db88efa79ddc1095a3ed292dac09e704341
oh-taehyun <xogus7038@naver.com>
2023-05-29 13:09:51 +0900

    모음 제거

---
+++ codingtest/programmers/src/모음제거.java
@@ -0,0 +1,16 @@
+
+public class 모음제거 {
+	public String solution(String my_string) {
+		String replace[]= {"a","e","i","o","u"};
+		String answer="";
+		for(int i=0;i<replace.length;i++) {
+			if(my_string.contains(replace[i])) {
+				answer=my_string.replace(replace[i], "");
+				my_string=answer;
+			}else {
+				answer=my_string;
+			}
+		}
+		return answer;
+	}
+}
