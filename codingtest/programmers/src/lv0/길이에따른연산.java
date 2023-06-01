40f220fef3dc3bd9d8c3af9df454e84120d7b04b
oh-taehyun <xogus7038@naver.com>
2023-06-01 23:02:23 +0900

    길이에 따른 연산

---
+++ codingtest/programmers/src/lv0/길이에따른연산.java
@@ -0,0 +1,18 @@
+package lv0;
+
+public class 길이에따른연산 {
+	public int solution(int[]num_list) {
+		int answer=0;
+		if(num_list.length>=11) {
+			for(int i=0;i<num_list.length;i++) {
+				answer=num_list[i]+answer;
+			}
+		}else if(num_list.length<=10) {
+			answer=1;
+			for(int i=0;i<num_list.length;i++) {
+				answer=answer*num_list[i];
+			}
+		}
+		return answer;
+	}
+}
