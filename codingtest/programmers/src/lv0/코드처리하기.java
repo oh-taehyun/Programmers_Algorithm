44b326c8d497a4978a6c8155b70ba433ae7076b4
oh-taehyun <xogus7038@naver.com>
2023-06-09 00:01:19 +0900

    코드처리하기 주석추가

--- codingtest/programmers/src/lv0/코드처리하기.java
+++ codingtest/programmers/src/lv0/코드처리하기.java
@@ -5,8 +5,8 @@ public class 코드처리하기 {
         String answer = "";
         int mode = 0;
         for (int i = 0; i < code.length(); i++) {
-            if (code.charAt(i) == '1') mode = 1 - mode;
-            else if (i % 2 == mode) answer += code.charAt(i);
+            if (code.charAt(i) == '1') mode = 1 - mode;//mode가 1이라면 0으로 바뀌고 0이라면 1로 바뀜
+            else if (i % 2 == mode) answer += code.charAt(i);//mode가 1일 때 홀수이므로 code[i]가 더해지고, mode가 0일 때 짝수이므로 code[i]가 더해짐
         }
         return "".equals(answer) ? "EMPTY" : answer;
     }
