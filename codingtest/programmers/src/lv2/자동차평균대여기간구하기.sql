d44acb17dba3d76a4a57631612792bf26f9b9e9c
oh-taehyun <xogus7038@naver.com>
2023-09-09 23:45:58 +0900

    23. 9. 9. 자동차 평균 대여기간 구하기

---
+++ codingtest/programmers/src/lv2/자동차평균대여기간구하기.sql
@@ -0,0 +1,4 @@
+SELECT car_id, ROUND(AVG(end_date-start_date+1),1) AS
+AVERAGE_DURATION FROM car_rental_company_rental_history GROUP BY
+car_id HAVING ROUND(AVG(end_date-start_date+1),1)>=7 ORDER BY average_duration
+DESC, car_id DESC;
