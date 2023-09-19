0a900612af9cc59f408811fd3518c8c73b7b5043
oh-taehyun <xogus7038@naver.com>
2023-09-19 10:18:34 +0900

    23. 9. 19. 문자열 돌리기(charAt())

---
+++ codingtest/programmers/src/lv0/문자열돌리기.java
@@ -0,0 +1,13 @@
+package lv0;
+
+import java.util.Scanner;
+
+public class 문자열돌리기 {
+	public static void main(String[] args ) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		for(int i=0;i<a.length();i++) {
+			System.out.println(a.charAt(i));
+		}
+	}
+}
